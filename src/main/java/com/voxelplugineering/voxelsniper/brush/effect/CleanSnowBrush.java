209c5468818a7b2c4273db3af9a4bf7221d380e7
Nick Conde <nick@budgiehouse.com>
2015-08-09 22:51:31 -0700

    Fix cleansnow brush

--- src/main/java/com/voxelplugineering/voxelsniper/brush/effect/CleanSnowBrush.java
+++ src/main/java/com/voxelplugineering/voxelsniper/brush/effect/CleanSnowBrush.java
@@ -23,18 +23,15 @@
  */
 package com.voxelplugineering.voxelsniper.brush.effect;
 
-import com.google.common.base.Optional;
-import com.voxelplugineering.voxelsniper.GunsmithLogger;
 import com.voxelplugineering.voxelsniper.brush.Brush;
 import com.voxelplugineering.voxelsniper.brush.BrushInfo;
 import com.voxelplugineering.voxelsniper.brush.BrushKeys;
-import com.voxelplugineering.voxelsniper.brush.BrushParam;
 import com.voxelplugineering.voxelsniper.brush.BrushPartType;
 import com.voxelplugineering.voxelsniper.brush.BrushVars;
 import com.voxelplugineering.voxelsniper.brush.ExecutionResult;
-import com.voxelplugineering.voxelsniper.config.VoxelSniperConfiguration;
 import com.voxelplugineering.voxelsniper.entity.Player;
 import com.voxelplugineering.voxelsniper.service.registry.MaterialRegistry;
+import com.voxelplugineering.voxelsniper.service.text.TextFormat;
 import com.voxelplugineering.voxelsniper.shape.MaterialShape;
 import com.voxelplugineering.voxelsniper.shape.Shape;
 import com.voxelplugineering.voxelsniper.shape.SingleMaterialShape;
@@ -42,68 +39,90 @@ import com.voxelplugineering.voxelsniper.util.Context;
 import com.voxelplugineering.voxelsniper.util.brush.BrushVarsHelper;
 import com.voxelplugineering.voxelsniper.world.Block;
 import com.voxelplugineering.voxelsniper.world.Location;
+import com.voxelplugineering.voxelsniper.world.material.Material;
 import com.voxelplugineering.voxelsniper.world.material.MaterialState;
 import com.voxelplugineering.voxelsniper.world.queue.ShapeChangeQueue;
+
+import com.google.common.base.Optional;
 
 /**
  * An effect brush that removes snow layers that are on top of snow layers.
  */
-@BrushInfo(name = "clearsnow", type = BrushPartType.EFFECT, help = "Removes floating snow blocks", params = {
-    @BrushParam(name = BrushKeys.FILL, desc = "Whether to replace with snow blocks")
-})
-public class ClearSnowBrush extends Brush {
+@BrushInfo(name = "cleansnow", type = BrushPartType.EFFECT, help = "Removes floating snow blocks", permission = "voxelsniper.brush.cleansnow")
+public class CleanSnowBrush extends Brush
+{
+
+    private static final String[] SNOW_NAMES = new String[] { "snow_layer", "snow" };
 
     private MaterialState air;
-    private MaterialState snow;
-    private MaterialState snowLayer;
+    private Material snowLayer;
 
     @Override
-    public void init(Context context) {
+    public void init(Context context)
+    {
         MaterialRegistry<?> registry = context.getRequired(MaterialRegistry.class);
         this.air = registry.getAirMaterial().getDefaultState();
-        this.snow = registry.getMaterial("snow").get().getDefaultState();
-        this.snowLayer = registry.getMaterial("snow_layer").get().getDefaultState();
+        for (String n : SNOW_NAMES)
+        {
+            Optional<Material> attempt = registry.getMaterial(n);
+            if (attempt.isPresent())
+            {
+                this.snowLayer = attempt.get();
+            }
+        }
     }
 
     @Override
-    public ExecutionResult run(Player player, BrushVars args) {
+    public ExecutionResult run(Player player, BrushVars args)
+    {
+        if (this.snowLayer == null || this.snowLayer == null)
+        {
+            player.sendMessage(TextFormat.DARK_RED + "Failed to find snow materials, brush will not funtion.");
+            return ExecutionResult.abortExecution();
+        }
         Optional<Shape> s = args.get(BrushKeys.SHAPE, Shape.class);
-        if (!s.isPresent()) {
+        if (!s.isPresent())
+        {
             player.sendMessage("You must have at least one shape brush before your material brush.");
             return ExecutionResult.abortExecution();
         }
-        boolean fill = args.get(BrushKeys.FILL, Boolean.class).or(VoxelSniperConfiguration.fillDefault);
         Optional<Block> l = BrushVarsHelper.getTargetBlock(args);
         Location loc = l.get().getLocation();
-        MaterialShape ms = new SingleMaterialShape(s.get(), fill ? this.snow : this.air);
-        for (int x = 0; x < ms.getWidth(); x++) {
+        MaterialShape ms = new SingleMaterialShape(s.get(), this.air);
+        for (int x = 0; x < ms.getWidth(); x++)
+        {
             int x0 = loc.getFlooredX() + x - ms.getOrigin().getX();
-            for (int z = 0; z < ms.getLength(); z++) {
+            for (int z = 0; z < ms.getLength(); z++)
+            {
                 int z0 = loc.getFlooredZ() + z - ms.getOrigin().getZ();
-                for (int y = fill ? 0 : ms.getHeight() -1; fill ? y < ms.getHeight() -1 : y > 0; y += fill ? 1 : -1) {
+                for (int y = ms.getHeight() - 1; y > 0; y--)
+                {
                     int y0 = loc.getFlooredY() + y - ms.getOrigin().getY();
-                    if (!ms.get(x, y, z, false)) {
+                    if (!ms.get(x, y, z, false))
+                    {
                         continue;
                     }
                     Optional<Block> oblock = player.getWorld().getBlock(x0, y0, z0);
-                    if (!oblock.isPresent()) {
-                        GunsmithLogger.getLogger().info("Failed to get block, unsetting");
+                    if (!oblock.isPresent())
+                    {
                         ms.unset(x, y, z, false);
                         continue;
                     }
                     MaterialState mat = oblock.get().getMaterial();
-                    if (!mat.getType().equals(this.snowLayer)) {
+                    if (!mat.getType().equals(this.snowLayer))
+                    {
                         ms.unset(x, y, z, false);
                         continue;
                     }
-                    Optional<Block> oblock1 = player.getWorld().getBlock(x0, y0 + (fill ? 1 : -1), z0);
-                    if (!oblock.isPresent()) {
-                        GunsmithLogger.getLogger().info("Failed to get block, unsetting");
+                    Optional<Block> oblock1 = player.getWorld().getBlock(x0, y0 - 1, z0);
+                    if (!oblock1.isPresent())
+                    {
                         ms.unset(x, y, z, false);
                         continue;
                     }
-                    MaterialState mat1 = oblock.get().getMaterial();
-                    if (!mat1.getType().equals(this.snowLayer)) {
+                    MaterialState mat1 = oblock1.get().getMaterial();
+                    if (!mat1.getType().equals(this.snowLayer))
+                    {
                         ms.unset(x, y, z, false);
                     }
                 }
